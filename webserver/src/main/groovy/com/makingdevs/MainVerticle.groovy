b977cae9d519acec77f98940b8c894ec8b9faf2f
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-12-12 09:37:01 -0600

    Usamos Vertx Web

--- webserver/src/main/groovy/com/makingdevs/MainVerticle.groovy
+++ webserver/src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -2,16 +2,58 @@ package com.makingdevs
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.DeploymentOptions
+import io.vertx.ext.web.Router
 
 class MainVerticle extends AbstractVerticle {
 
   @Override
   void start() {
-    println "Desplegando Verticles"
+    println "Iniciando servidor HTTP..."
 
-    //DeploymentOptions options = new DeploymentOptions().setInstances(10)
+    Router router = Router.router(vertx);
 
-    vertx.deployVerticle("com.makingdevs.SenderVerticle")
-    vertx.deployVerticle("com.makingdevs.ReceiverVerticle")
+    configureRoutes(router)
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8083) { http ->
+        if(http.succeeded())
+          println "Servidor HTTP en http://localhost:8083"
+        else
+          println "Error al iniciar el server: ${http.cause()}"
+      }
+  }
+
+  private void configureRoutes(Router router) {
+
+    router.get('/').handler { ctx ->
+      ctx.response()
+        .putHeader('Content-type', 'text/plain')
+        .end('Bienvenido a la API REST con Vert.x y Groovy')
+    }
+
+    router.get('/products').handler { ctx ->
+      ctx.response().end("Lista de productos")
+    }
+
+    router.get('/products/:id').handler { ctx ->
+      String producId = ctx.pathParam("id")
+      ctx.response().end("Producto ${producId}")
+    }
+
+    router.post('/products').handler { ctx ->
+      ctx.response().setStatusCode(201).end("Producto creado")
+    }
+
+    router.put('/products/:id').handler { ctx ->
+      String producId = ctx.pathParam("id")
+      ctx.response().end("Producto ${producId} Actualizado")
+    }
+
+    router.delete('/products/:id').handler { ctx ->
+      String producId = ctx.pathParam("id")
+      ctx.response().end("Producto ${producId} Eliminado")
+    }
+
   }
 }
