4577b818d44644c70caaf757559b8991258e4bbe
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-12-12 10:07:58 -0600

    Uso de Vert.x Web para API Rest

--- webserver/src/main/groovy/com/makingdevs/MainVerticle.groovy
+++ webserver/src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -3,14 +3,22 @@ package com.makingdevs
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.DeploymentOptions
 import io.vertx.ext.web.Router
+import io.vertx.core.json.JsonObject
 
 class MainVerticle extends AbstractVerticle {
+
+  private Map<Integer, JsonObject> database = [:]
 
   @Override
   void start() {
     println "Iniciando servidor HTTP..."
 
     Router router = Router.router(vertx);
+
+    router.route('/products*').handler { ctx ->
+      println ctx.request().properties
+      ctx.next()
+    }
 
     configureRoutes(router)
 
@@ -33,16 +41,38 @@ class MainVerticle extends AbstractVerticle {
     }
 
     router.get('/products').handler { ctx ->
-      ctx.response().end("Lista de productos")
+      ctx.response()
+        .putHeader('Content-Type', 'application/json')
+        .end(database.values().toString())
     }
 
     router.get('/products/:id').handler { ctx ->
-      String producId = ctx.pathParam("id")
-      ctx.response().end("Producto ${producId}")
+      Integer producId = ctx.pathParam("id").toInteger()
+      JsonObject product = database[producId]
+      if(product) {
+        ctx.response()
+          .putHeader('Content-Type', 'application/json')
+          .end(product.encodePrettily())
+      } else {
+        ctx.response()
+          .setStatusCode(404)
+          .end("Producto con ID ${producId} no encontrado")
+      }
     }
 
     router.post('/products').handler { ctx ->
-      ctx.response().setStatusCode(201).end("Producto creado")
+      ctx.request().bodyHandler { body ->
+        JsonObject newProduct = body.toJsonObject()
+
+        int id = database.size() + 1
+        newProduct.put('id', id)
+        database[id] = newProduct
+
+        ctx.response()
+          .setStatusCode(201)
+          .putHeader('Content-Type', 'application/json')
+          .end(newProduct.encodePrettily())
+      }
     }
 
     router.put('/products/:id').handler { ctx ->
